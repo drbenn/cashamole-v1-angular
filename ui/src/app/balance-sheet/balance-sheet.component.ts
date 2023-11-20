c3360c53bc11e7083d05a703c828c091a26f1feb
drbenn <bennett.daniel@gmail.com>
2023-11-19 22:23:13 -0500

    basic component structure added

---
+++ ui/src/app/balance-sheet/balance-sheet.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-balance-sheet',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './balance-sheet.component.html',
+  styleUrl: './balance-sheet.component.scss'
+})
+export class BalanceSheetComponent {
+
+}
