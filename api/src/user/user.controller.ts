ed0dd562ad24aaad0033994164d78f3fd57383a5
drbenn <bennett.daniel@gmail.com>
2023-11-20 23:01:14 -0500

    login-user api bug fix

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Header, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
-import { InsertUser, RegisterUserDto, UserExist } from './register-user-dto/register-user-dto';
+import { InsertUser, LoginUserDto, RegisterUserDto, UserExist } from './register-user-dto/register-user-dto';
 import { Response } from 'express';
 
 
@@ -40,21 +40,21 @@ export class UserController {
     @Post('login_user')
     async loginUser(
         @Res() res: Response, 
-        @Body() userRegisterDto: RegisterUserDto
+        @Body() userLoginDto: LoginUserDto
         ) {
-        // let users = this.userService.findAll();
-        const email: string = userRegisterDto.email;
-        const username: string = userRegisterDto.username;
-        const isUserExisting: UserExist = await this.userService.doesUserExist(email,username);
+        const username: string = userLoginDto.username;
+        const password: string = userLoginDto.password;
+        const loginValidation = await this.userService.validateLoginCredentials(userLoginDto);
+        // const isUserExisting: UserExist = await this.userService.doesUserExist(email,username);
 
-        if (!isUserExisting.userExist) {
-            const insertResponse: InsertUser = await this.userService.insertNewUser(userRegisterDto);
-            res.status(HttpStatus.ACCEPTED).send("successful" + JSON.stringify(insertResponse));
-            // todo: send registration confirmation email
-            // todo: create user specific database tables
-        } else {
-            res.status(HttpStatus.NOT_ACCEPTABLE).send("failed" + JSON.stringify(isUserExisting));
-        };
+        // if (!isUserExisting.userExist) {
+        //     const insertResponse: InsertUser = await this.userService.insertNewUser(userRegisterDto);
+        //     res.status(HttpStatus.ACCEPTED).send("successful" + JSON.stringify(insertResponse));
+        //     // todo: send registration confirmation email
+        //     // todo: create user specific database tables
+        // } else {
+        //     res.status(HttpStatus.NOT_ACCEPTABLE).send("failed" + JSON.stringify(isUserExisting));
+        // };
     };
 
 
@@ -78,17 +78,5 @@ export class UserController {
 //     }
 //   )
   
-  // Helper function for app.post('/loginuser')
-//   async function validateLoginCredentials(username, password) {
-//     const sql = `SELECT * FROM users WHERE username= ? AND password= ?`;
-//     const result = await con.promise().query(sql, [username, password])
-//       .then( ([row, fields]) => {
-//         console.log(row);
-//         console.log(fields);
-//         const isLoginValidated = row && row.length === 1;
-//         const userId = 0; // todo: once userId is column in table, should be row.id
-//         return { userId: userId, isLoginValidated: isLoginValidated}
-//       });
-//     return result;
-//   }
+
 }
