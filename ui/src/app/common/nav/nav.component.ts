8dca89b6c6a131ecaec209a54b4e037be91e5d65
drbenn <bennett.daniel@gmail.com>
2023-11-24 23:00:45 -0500

    nav styling

--- ui/src/app/common/nav/nav.component.ts
+++ ui/src/app/common/nav/nav.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { UserApiService } from '../../shared/api/user-api.service';
-import { first, take } from 'rxjs';
+import { Observable, first, take } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { UserStateModel } from '../../store/user/userState.state';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,26 @@ import { first, take } from 'rxjs';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-  private userApi = inject(UserApiService)
+  private userApi = inject(UserApiService);
+  @Select((state: {user: UserStateModel}) => state.user.loggedInUser.username)
+  loggedInUser$! : Observable<string>;
+
+public loggedInUserVal!: string;
+
+
+
+ngOnInit(): void {
+    this.loggedInUser$.subscribe((username:string) => this.loggedInUserVal = username);
+}
+
+  constructor(
+    private router: Router,
+    private store: Store
+    ) {}
+
+  protected navigateHome() {
+    this.router.navigate(['home']);
+  }
 
   protected logoutUser(): void {
     this.userApi.logoutUser().pipe(take(1), first())
