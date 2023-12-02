73d0e2faf37757cee9ad891bdab76e34bef578f3
“drbenn” <bennett.daniel@gmail.com>
2023-12-01 22:15:11 -0500

    more restructuring income/expense now in core

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './core/home/home.component';
-import { UserLoginComponent } from './auth/user-login/user-login.component';
 import { UserRegisterComponent } from './auth/user-register/user-register.component';
+import { UserLoginComponent } from './auth/user-login/user-login.component';
 import { UserResetPasswordComponent } from './auth/user-reset-password/user-reset-password.component';
 import { DonateComponent } from './shared/donate/donate.component';
 import { HowToComponent } from './shared/how-to/how-to.component';
-import { AppComponent } from './app.component';
 import { NewTransactionComponent } from './core/transactions/new-transaction/new-transaction.component';
 import { NewBsRecordComponent } from './core/balance-sheet/new-bs-record/new-bs-record.component';
+import { HomeComponent } from './core/home/home.component';
 import { SplashComponent } from './shared/splash/splash.component';
-import { NewExpenseTransactionComponent } from './core/transactions/new-expense-transaction/new-expense-transaction.component';
-import { NewIncomeTransactionComponent } from './core/transactions/new-income-transaction/new-income-transaction.component';
+import { NewIncomeTransactionComponent } from './core/income/new-income-transaction/new-income-transaction.component';
+import { NewExpenseTransactionComponent } from './core/expense/new-expense-transaction/new-expense-transaction.component';
+
 
 export const routes: Routes = [
     // { path: '', component: HomeComponent },
--- ui/src/app/core/transactions/transactions.component.ts
+++ ui/src/app/core/transactions/transactions.component.ts
@@ -5,8 +5,8 @@ import { TransactionBody } from '../../model/transaction.model';
 import { Store } from '@ngxs/store';
 import { TransactionTableComponent } from './transaction-table/transaction-table.component';
 import { NewTransactionComponent } from './new-transaction/new-transaction.component';
-import { ExpenseTableComponent } from './expense-table/expense-table.component';
-import { IncomeTableComponent } from './income-table/income-table.component';
+import { ExpenseTableComponent } from '../expense/expense-table/expense-table.component';
+import { IncomeTableComponent } from '../income/income-table/income-table.component';
 
 @Component({
   selector: 'app-transactions',
@@ -16,56 +16,56 @@ import { IncomeTableComponent } from './income-table/income-table.component';
   styleUrl: './transactions.component.scss'
 })
 export class TransactionsComponent implements OnInit {
-  transactions$: Observable<any> = this.store.select((state) => state.user.transactions);
-  // tableCategories: Set<string> | [] = new Set();
-  // transactionSets: any = {};
+  // transactions$: Observable<any> = this.store.select((state) => state.user.transactions);
+  // // tableCategories: Set<string> | [] = new Set();
+  // // transactionSets: any = {};
 
-  expenseCategorySet: Set<string> = new Set();
-  expenseCategoryArray: string[] = [];
-  expenseTransactionObjects: any = {};
+  // expenseCategorySet: Set<string> = new Set();
+  // expenseCategoryArray: string[] = [];
+  // expenseTransactionObjects: any = {};
 
-  incomeTransactionObjects: any = [];
+  // incomeTransactionObjects: any = [];
 
 
-  constructor(private store: Store) {}
+  // constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.getAndSetTransactions();
+    // this.getAndSetTransactions();
   };
 
-  private getAndSetTransactions(): void {
-    this.transactions$.subscribe((entries: TransactionBody[]) => {
-      if (entries) {
-        console.log(entries);
+  // private getAndSetTransactions(): void {
+  //   this.transactions$.subscribe((entries: TransactionBody[]) => {
+  //     if (entries) {
+  //       console.log(entries);
         
-        entries.forEach((entry: TransactionBody) => {
-          // income hit first because category field may be undefined
-          if (entry.type === 'income') {
-            this.incomeTransactionObjects.push(entry);
-          }
-          if (entry.type === 'expense') {
-            console.log(entry);
-            this.expenseCategorySet.add(entry.category);
-            this.expenseCategoryArray = Array.from(this.expenseCategorySet);
-            if (!this.expenseTransactionObjects[entry.category]) {
-              this.expenseTransactionObjects[entry.category] = [];
-            } 
-            this.expenseTransactionObjects[entry.category].push(entry);
-          } 
+  //       entries.forEach((entry: TransactionBody) => {
+  //         // income hit first because category field may be undefined
+  //         if (entry.type === 'income') {
+  //           this.incomeTransactionObjects.push(entry);
+  //         }
+  //         if (entry.type === 'expense') {
+  //           console.log(entry);
+  //           this.expenseCategorySet.add(entry.category);
+  //           this.expenseCategoryArray = Array.from(this.expenseCategorySet);
+  //           if (!this.expenseTransactionObjects[entry.category]) {
+  //             this.expenseTransactionObjects[entry.category] = [];
+  //           } 
+  //           this.expenseTransactionObjects[entry.category].push(entry);
+  //         } 
           
 
-        });
+  //       });
 
-      }
-      console.log(this.expenseCategoryArray);
-      console.log(this.expenseTransactionObjects);
-      console.log(this.incomeTransactionObjects);
+  //     }
+  //     console.log(this.expenseCategoryArray);
+  //     console.log(this.expenseTransactionObjects);
+  //     console.log(this.incomeTransactionObjects);
       
       
       
-    },
-      (error: any )=> console.log(error)
-    );
-  }
+  //   },
+  //     (error: any )=> console.log(error)
+  //   );
+  // }
 
 }
