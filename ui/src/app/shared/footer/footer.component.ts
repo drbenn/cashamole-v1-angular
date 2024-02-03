b3e05aae14188fb6d657807da320653af336c2b8
“drbenn” <bennett.daniel@gmail.com>
2024-02-03 11:21:52 -0500

    footer work claim and linking

--- ui/src/app/shared/footer/footer.component.ts
+++ ui/src/app/shared/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TooltipModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
