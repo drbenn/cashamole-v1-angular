af4e48ab7d4802b0e73ce80dcb85fb33e7554567
“drbenn” <bennett.daniel@gmail.com>
2024-02-09 13:07:23 -0500

    lazy load routing, wildcard route reroute

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,30 +1,31 @@
 import { Routes } from '@angular/router';
-import { UserRegisterComponent } from './pages/user-register/user-register.component';
-import { UserLoginComponent } from './pages/user-login/user-login.component';
-import { UserResetPasswordComponent } from './pages/user-reset-password/user-reset-password.component';
-import { DonateComponent } from './pages/donate/donate.component';
-import { HowToComponent } from './pages/how-to/how-to.component';
-import { SplashComponent } from './pages/splash/splash.component'
 import { BalanceSheetComponent } from './pages/balance-sheet/balance-sheet.component';
-import { IncomeComponent } from './pages/income/income.component';
-import { ExpenseComponent } from './pages/expense/expense.component';
-import { InvestComponent } from './pages/invest/invest.component';
-import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DonateComponent } from './pages/donate/donate.component';
+import { ExpenseComponent } from './pages/expense/expense.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HowToComponent } from './pages/how-to/how-to.component';
+import { IncomeComponent } from './pages/income/income.component';
+import { InvestComponent } from './pages/invest/invest.component';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { UserResetPasswordComponent } from './pages/user-reset-password/user-reset-password.component';
+import { SplashComponent } from './pages/splash/splash.component'
 
 
 export const routes: Routes = [
     { path: '', component: UserLoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'balance-sheeet', component: BalanceSheetComponent },
-    { path: 'income', component: IncomeComponent },
-    { path: 'invest', component: InvestComponent },
-    { path: 'expense', component: ExpenseComponent },
-    { path: 'login', component: UserLoginComponent },
-    { path: 'register', component: UserRegisterComponent },
-    { path: 'password-reset', component: UserResetPasswordComponent },
-    { path: 'donate', component: DonateComponent },
-    { path: 'how-to', component: HowToComponent },
-    { path: 'splash', component: SplashComponent },
+    { path: 'balance-sheeet', loadComponent: () => import('./pages/balance-sheet/balance-sheet.component').then(m => m.BalanceSheetComponent) },
+    { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+    { path: 'donate', loadComponent: () => import('./pages/donate/donate.component').then(m => m.DonateComponent) },
+    { path: 'expense', loadComponent: () => import('./pages/expense/expense.component').then(m => m.ExpenseComponent) },
+    { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'how-to', loadComponent: () => import('./pages/how-to/how-to.component').then(m => m.HowToComponent) },
+    { path: 'income', loadComponent: () => import('./pages/income/income.component').then(m => m.IncomeComponent) },
+    { path: 'invest', loadComponent: () => import('./pages/invest/invest.component').then(m => m.InvestComponent) },
+    { path: 'login', loadComponent: () => import('./pages/user-login/user-login.component').then(m => m.UserLoginComponent) },
+    { path: 'register', loadComponent: () => import('./pages/user-register/user-register.component').then(m => m.UserRegisterComponent) },
+    { path: 'password-reset', loadComponent: () => import('./pages/user-reset-password/user-reset-password.component').then(m => m.UserResetPasswordComponent) },
+    { path: 'splash', loadComponent: () => import('./pages/splash/splash.component').then(m => m.SplashComponent) },
+    { path: '**', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
 ];
